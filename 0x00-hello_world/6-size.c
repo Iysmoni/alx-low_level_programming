3cb6e94775826939073f685db8dfec122b12ef99
“iysmoni” <matthewheloo@gmail.com>
2023-02-16 18:47:31 -0800

    .

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - size of various types of computers
+ *
+ * Return: (0) if successful
+ *
+ */
+int main(void)
+{
+	printf("Size of char: %lu  byte(s)", sizeof(char));
+	printf("Size of an int: %lu byte(s)", sizeof(int));
+	printf("Size of a long int: %lu byte(s)", sizeof(long int));
+	printf("Size of long long int: %lu byte(s)", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
+}
