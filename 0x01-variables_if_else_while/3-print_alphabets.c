c1fb76394e21c5d5129d7a0d6fb85806fd17244a
“iysmoni” <matthewheloo@gmail.com>
2023-02-18 12:17:08 -0500

    .

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,11 @@
 int main(void)
 {
 	char i;
-	for (i = 'a' ; <= 'z' ; i++)
+
+	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	for (i = 'A' ; <= 'Z' ; i++)
+	for (i = 'A' ; i <= 'Z' ; i++)
 		putchar(i);
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
