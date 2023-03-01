956ec3752dded5ffaadc9fa174009935df4f15ff
“iysmoni” <matthewheloo@gmail.com>
2023-03-01 12:04:36 -0500

    .

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* more_numbers - prints 10 times the numbers, from 0 to 14
-* followed by a new line
+* more_numbers - prints 10x the numbers, from 0 to 14
+* make  a new line
 */
 
 void more_numbers(void)
